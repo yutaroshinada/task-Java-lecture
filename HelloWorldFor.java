c689314f6c73a4caf247b216101dd777cb3235ba
yutaroshinada <105104221+yutaroshinada@users.noreply.github.com>
2022-05-14 03:32:11 +0900

    Add files via upload

---
+++ HelloWorldFor.java
@@ -0,0 +1,13 @@
+package task1;
+
+public class HelloWorldFor {
+    public static void main(String[] args) {
+        for(int i  = 1;i < 12;i++){
+            if (i < 11){
+                System.out.println("HelloWorld" + i + "回目");
+            }else{
+                System.out.println("Hello");
+            }
+        }
+    }
+}
